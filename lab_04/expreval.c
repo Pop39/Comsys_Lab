1549f8fd361343b37957697b33ecf5ddb6ff32e8
Supasan Komonlit <supasan_224@hotmail.com>
2019-02-25 11:43:05 +0700

    Update

--- lab_04/expreval.c
+++ lab_04/expreval.c
@@ -2,6 +2,8 @@
 #include	<stdio.h>
 #include	<assert.h>
 
+#define _PRINT_DATA_ALL_FUNCTION_
+
 static FILE *f;
 static int ch;
 static unsigned int val;
@@ -27,12 +29,15 @@ static void Number(){
 		val = val * 10 + ch - '0';
 		ch = getc( f );
 	}
+#ifdef _PRINT_DATA_ALL_FUNCTION_
+	printf("Function number val is %5d\n" , val );
+#endif
 }
 
 // This function we determine type of data in case that is number will collect in val
 static int SGet(){
 	register int sym;
-	
+
 	while( (ch != EOF ) && ( ch <= ' ' ) ) ch = getc(f);
 
 	switch( ch ){
@@ -41,25 +46,35 @@ static int SGet(){
 		case '-' : sym = minus; ch = getc(f); break;
 		case '*' : sym = times; ch = getc(f); break;
 		case '/' : sym = divide; ch = getc(f); break;
-		case '&' : sym = mod; ch = getc(f); break;
+		case '%' : sym = mod; ch = getc(f); break;
 		case '(' : sym = lparen; ch = getc(f); break;
 		case ')' : sym = rparen; ch = getc(f); break;
-		case '0' : case '1' : case '2' : case '3' : case '4' :
-		case '5' : case '6' : case '7' : case '8' : case '9' :
-				sym = number; Number(); break;
+
+		case '0' : case '1' : case '2' : case '3' : case '4' : case '5' : case '6' : case '7' : 
+		case '8' : case '9' : sym = number; break;
 		default  : sym = illegal;
 	}
+	
+	#ifdef _PRINT_DATA_ALL_FUNCTION_
+		printf("Function SGet ch  is   : %5d\n" , ch );
+		printf("Function SGet sym is   : %5d\n" , sym );		
+	#endif
 
+	if( sym == number ) Number();
+	
 	return sym;
 }
 
 // This function is expression
 static void Expr(){
+	int type_number = 1;
 	if( (sym == minus ) || (sym == plus) ){
+		if( sym == minus ) type_number = -1;
 		sym = SGet();
 	}
 
 	Term();
+	int old_val = val * type_number;
 
 	while( ( sym == plus ) || ( sym == minus ) ){
 		sym = SGet();
@@ -67,12 +82,15 @@ static void Expr(){
 	}
 }
 
-
 // Function assert use to aborted program when argument is 0
 static void Factor(){
+	int old_val;
 	assert( ( sym == number ) || ( sym == lparen ) );
 	if( sym == number ){
 		sym = SGet();
+		#ifdef _PRINT_DATA_ALL_FUNCTION_
+			printf("Factor Function result val is %5d" , val );
+		#endif
 	}
 	else{
 		sym = SGet();
@@ -84,9 +102,42 @@ static void Factor(){
 
 static void Term(){
 	Factor();
+	int old_val , old_sym;
 	while( ( sym == times ) || ( sym == divide ) || (sym == mod ) ){
+		old_val = val;
+		old_sim = sym;
 		sym = SGet();
 		Factor();
+		if( old_sym == time ){
+			#ifdef _PRINT_DATA_ALL_FUNCTION_
+				printf("Factor Function old_val is %5d" , old_val );
+				printf("Factor Function val is %5d" , val );
+			#endif
+			val = old_val * val;
+			#ifdef _PRINT_DATA_ALL_FUNCTION_
+				printf("Factor Function result val is %5d" , val );
+			#endif
+		}
+		else if( old_sym == divide ){
+			#ifdef _PRINT_DATA_ALL_FUNCTION_
+				printf("Factor Function old_val is %5d" , old_val );
+				printf("Factor Function val is %5d" , val );
+			#endif
+			val = old_val / val;
+			#ifdef _PRINT_DATA_ALL_FUNCTION_
+				printf("Factor Function result val is %5d" , val );
+			#endif
+		}
+		else{
+			#ifdef _PRINT_DATA_ALL_FUNCTION_
+				printf("Factor Function old_val is %5d" , old_val );
+				printf("Factor Function val is %5d" , val );
+			#endif
+			val = old_val % val;
+			#ifdef _PRINT_DATA_ALL_FUNCTION_
+				printf("Factor Function result val is %5d" , val );
+			#endif
+		}
 	}
 }
 
