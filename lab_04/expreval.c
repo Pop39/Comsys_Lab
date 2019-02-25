8df326aa6dbbd58a775a5ea7ab62b0a6c49ac189
Supasan Komonlit <supasan_224@hotmail.com>
2019-02-25 10:42:03 +0700

    Write follow pdf

--- lab_04/expreval.c
+++ lab_04/expreval.c
@@ -1,2 +1,106 @@
 #include	<stdlib.h>
 #include	<stdio.h>
+#include	<assert.h>
+
+static FILE *f;
+static int ch;
+static unsigned int val;
+enum { plus, minus, times, divide, mod, lparen, rparen, number, eof, illegal};
+static int sym;
+
+static void Expr();
+static void Factor();
+static void Term();
+
+// This function use for readfile into variable f
+static void SInit( char* filename ){ 
+	ch = EOF;
+	f = fopen( filename , "r+t" ); // open text file for reading and writing
+	if( f!= NULL ) ch = getc(f); 
+	// function getc is get character from stream in this situation stream file is f variable
+}
+
+// This function use for get value of number
+static void Number(){
+	val = 0;
+	while( ('0' <= ch ) && ( ch <= '9' ) ){
+		val = val * 10 + ch - '0';
+		ch = getc( f );
+	}
+}
+
+// This function we determine type of data in case that is number will collect in val
+static int SGet(){
+	register int sym;
+	
+	while( (ch != EOF ) && ( ch <= ' ' ) ) ch = getc(f);
+
+	switch( ch ){
+		case EOF : sym = eof; break;
+		case '+' : sym = plus; ch = getc(f); break;
+		case '-' : sym = minus; ch = getc(f); break;
+		case '*' : sym = times; ch = getc(f); break;
+		case '/' : sym = divide; ch = getc(f); break;
+		case '&' : sym = mod; ch = getc(f); break;
+		case '(' : sym = lparen; ch = getc(f); break;
+		case ')' : sym = rparen; ch = getc(f); break;
+		case '0' : case '1' : case '2' : case '3' : case '4' :
+		case '5' : case '6' : case '7' : case '8' : case '9' :
+				sym = number; Number(); break;
+		default  : sym = illegal;
+	}
+
+	return sym;
+}
+
+// This function is expression
+static void Expr(){
+	if( (sym == minus ) || (sym == plus) ){
+		sym = SGet();
+	}
+
+	Term();
+
+	while( ( sym == plus ) || ( sym == minus ) ){
+		sym = SGet();
+		Term();
+	}
+}
+
+
+// Function assert use to aborted program when argument is 0
+static void Factor(){
+	assert( ( sym == number ) || ( sym == lparem ) );
+	if( sym == number ){
+		sym = SGet();
+	}
+	else{
+		sym = SGet();
+		Expr();
+		assert( sym == rparem );
+		sym = SGet();
+	}
+}
+
+static void Term(){
+	Factor();
+	while( ( sym == times ) || ( sym == divide ) || (sym == mod ) ){
+		sym = SGet();
+		Factor();
+	}
+}
+
+int main( int argc , char* argv[] ){
+	register int result;
+	
+	if( argc == 2 ){
+		SInit( argv[] );
+		sym = SGet();
+		Expr();
+		assert( sym == eof );
+	}
+	else{
+		printf("usage : expreval <filenam>\n");
+	}
+	return 0;
+}
